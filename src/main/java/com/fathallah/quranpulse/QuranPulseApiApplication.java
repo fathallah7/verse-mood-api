00024f4a44fe67efd54596626c65cb8c693e46e9
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-04-03 15:21:59 +0200

    first commit

---
+++ src/main/java/com/fathallah/quranpulse/QuranPulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.fathallah.quranpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuranPulseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuranPulseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fathallah/quranpulse/QuranPulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fathallah.quranpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuranPulseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
