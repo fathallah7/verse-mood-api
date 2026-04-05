8b33f554131fb407ffda897485f08b736efd8f03
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-04-05 09:31:21 +0200

    Add AyahController for handling Ayah retrieval by Surah number

---
+++ src/main/java/com/fathallah/quranpulse/controller/AyahController.java
@@ -0,0 +1,25 @@
+package com.fathallah.quranpulse.controller;
+
+import com.fathallah.quranpulse.entity.Ayah;
+import com.fathallah.quranpulse.service.AyahService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ayahs")
+public class AyahController {
+    private final AyahService ayahService;
+
+    public AyahController(AyahService ayahService) {
+        this.ayahService = ayahService;
+    }
+
+    @GetMapping("/{surahNumber}")
+    public List<Ayah> getAyahsBySurahNumber(@PathVariable Integer surahNumber) {
+        return ayahService.getAyahsBySurahNumber(surahNumber);
+    }
+}
