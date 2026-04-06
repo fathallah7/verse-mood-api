35705996b7283084062dcd106f5ec65581fdfc69
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-04-06 18:33:34 +0200

    Refactor AyahService to return AyahResponse objects for Ayah retrieval

--- src/main/java/com/fathallah/quranpulse/service/AyahService.java
+++ src/main/java/com/fathallah/quranpulse/service/AyahService.java
@@ -1,5 +1,6 @@
 package com.fathallah.quranpulse.service;
 
+import com.fathallah.quranpulse.dto.response.AyahResponse;
 import com.fathallah.quranpulse.entity.Ayah;
 import com.fathallah.quranpulse.repository.AyahRepository;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,22 @@ public class AyahService {
         this.ayahRepository = ayahRepository;
     }
 
-    public List<Ayah> getAyahsBySurahNumber(Integer surahNumber) {
-        return ayahRepository.findBySurahNumber(surahNumber);
+    public List<AyahResponse> getAyahsBySurahNumber(Integer surahNumber) {
+        return ayahRepository.findBySurahNumber(surahNumber)
+                .stream()
+                .map(ayah -> new AyahResponse(
+                        ayah.getIndex(),
+                        ayah.getSurahNumber(),
+                        ayah.getAyahNumber(),
+                        ayah.getArabicText(),
+                        ayah.getTafsir() != null ? ayah.getTafsir().getArabicText() : null
+                ))
+                .toList();
     }
 
-    public Ayah getAyahBySurahNumberAndAyahNumber(Integer surahNumber, Integer ayahNumber) {
-        return ayahRepository.findBySurahNumberAndAyahNumber(surahNumber, ayahNumber)
+    public AyahResponse getAyahBySurahNumberAndAyahNumber(Integer surahNumber, Integer ayahNumber) {
+        Ayah ayah = ayahRepository.findBySurahNumberAndAyahNumber(surahNumber, ayahNumber)
                 .orElseThrow(() -> new RuntimeException("Ayah not found"));
+        return new AyahResponse(ayah.getIndex(), ayah.getSurahNumber(), ayah.getAyahNumber(), ayah.getArabicText(), ayah.getTafsir().getArabicText());
     }
 }
