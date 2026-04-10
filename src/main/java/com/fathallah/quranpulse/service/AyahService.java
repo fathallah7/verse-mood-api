d466475ca1c9dfb71a80238fdcdc852a65ebf2de
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-04-10 14:40:28 +0200

    Refactor AyahService to improve exception handling for Ayah and Surah retrieval

--- src/main/java/com/fathallah/quranpulse/service/AyahService.java
+++ src/main/java/com/fathallah/quranpulse/service/AyahService.java
@@ -2,6 +2,8 @@ package com.fathallah.quranpulse.service;
 
 import com.fathallah.quranpulse.dto.response.AyahResponse;
 import com.fathallah.quranpulse.entity.Ayah;
+import com.fathallah.quranpulse.exception.AyahNotFoundException;
+import com.fathallah.quranpulse.exception.SurahNotFoundException;
 import com.fathallah.quranpulse.repository.AyahRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,11 @@ public class AyahService {
     }
 
     public List<AyahResponse> getAyahsBySurahNumber(Integer surahNumber) {
+
+        if (surahNumber < 1 || surahNumber > 114) {
+            throw new SurahNotFoundException(surahNumber);
+        }
+
         return ayahRepository.findBySurahNumber(surahNumber)
                 .stream()
                 .map(ayah -> new AyahResponse(
@@ -30,7 +37,7 @@ public class AyahService {
 
     public AyahResponse getAyahBySurahNumberAndAyahNumber(Integer surahNumber, Integer ayahNumber) {
         Ayah ayah = ayahRepository.findBySurahNumberAndAyahNumber(surahNumber, ayahNumber)
-                .orElseThrow(() -> new RuntimeException("Ayah not found"));
+                .orElseThrow(() -> new AyahNotFoundException(surahNumber, ayahNumber));
         return new AyahResponse(ayah.getIndex(), ayah.getSurahNumber(), ayah.getAyahNumber(), ayah.getArabicText(), ayah.getTafsir().getArabicText());
     }
 }
